db86d62ce4550cb238c61321b4248e9ff25c51be
jdasol406 <jdasol406@gmail.com>
2024-10-22 02:45:10 +0900

    20241022 TodoUpdate component

--- src/components/TodoArray.jsx
+++ src/components/TodoArray.jsx
@@ -1,43 +1,15 @@
 import { useState } from 'react';
 import TodoUpdate from './TodoUpdate';
+import TodoMainList from './TodoMainList';
 
 const TodoArray = ({input, setInput, todos, setTodos}) => {
   const [input2, setInput2] = useState('');
   const [currentIndex, setCurrentIndex] = useState();
 
-
-  const updateFunk = (e) => {
-    const trimmedText = e.currentTarget.textContent.trim();
-    const editText = trimmedText.slice(0, -2);
-
-    setInput2(editText);
-    
-    const todoIndex = e.currentTarget.dataset.index;
-    setCurrentIndex(todoIndex);
-  };
-
-  const deleteTodo = (index, e) => {
-    e.stopPropagation();
-
-    const newTodos = todos.filter((_, todoIndex) => todoIndex !== index);
-    setTodos(newTodos);
-
-    setInput(''); 
-  };
-
   return (
     <>
       <div>
-      <div id='list'>
-          <ul>
-            {todos.map((todo, index) => (
-              <li data-index={index} onClick={updateFunk} key={index}>
-                {todo}
-                <button id='delete-btn' onClick={(e) => deleteTodo(index, e)}>삭제</button>
-              </li>
-            ))}
-          </ul>
-        </div>
+        <TodoMainList todos={todos} setTodos={setTodos} setInput2={setInput2} setCurrentIndex={setCurrentIndex}/>
         <div>
           <TodoUpdate todos={todos} setTodos={setTodos} input2={input2} setInput2={setInput2} currentIndex={currentIndex} setCurrentIndex={setCurrentIndex} />
         </div>
